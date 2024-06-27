f9b082a4a321d0cdbb8674d8f53d1072a006309c
Okabe Minato <166805519+hou-rai3@users.noreply.github.com>
2024-06-27 16:23:01 +0900

    改悪

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,9 @@ bool flag = false;
 int16_t speed_now = 0;
 int16_t target = 0;
 
-const float kp = 0.130;
-const float ki = 0.035;
-const float kd = 0.0055;
-// const float kp = 0.1;
-// const float ki = 0.035;
-// const float kd = 0.0;
+const float kp = 0.010;
+const float ki = 0.000;
+const float kd = 0.000;
 const float sample_time = 0.02; // 20ms sample time
 PID pid_controller(kp, ki, kd, sample_time);
 
@@ -31,26 +28,53 @@ void stop_motor(int zero)
         DATA[i + 1] = zero & 0xFF;    // 下位バイト
     }
 }
-void motor_control(int16_t target, int16_t speed_now, PID &pid_controller, CAN &can, CANMessage &msg, uint8_t *DATA, int DJI_ID, bool &flag)
+
+uint8_t *motor_control(int16_t target, int16_t speed_now, PID &pid_controller, CAN &can, CANMessage &msg, uint8_t *DATA, int DJI_ID, bool &flag)
 {
-    if (msg.id == 0x201)
-    {
-        speed_now = (msg.data[0] << 8) | msg.data[1];
-    }
-    float output = pid_controller.calculate(target, speed_now); // PID計算
-    int16_t motor_speed = static_cast<int16_t>(-output);        // 出力値を速度として使用
-    DATA[0] = (motor_speed >> 8) & 0xFF;                        // 上位バイト
-    DATA[1] = motor_speed & 0xFF;                               // 下位バイト
-    motor_speed = static_cast<int16_t>(output);
-    DATA[2] = (motor_speed >> 8) & 0xFF; // 上位バイト
-    DATA[3] = motor_speed & 0xFF;        // 下位バイト
-    motor_speed = static_cast<int16_t>(-output);
-    DATA[4] = (motor_speed >> 8) & 0xFF; // 上位バイト
-    DATA[5] = motor_speed & 0xFF;        // 下位バイト
-    motor_speed = static_cast<int16_t>(output);
-    DATA[6] = (motor_speed >> 8) & 0xFF; // 上位バイト
-    DATA[7] = motor_speed & 0xFF;        // 下位バイト
+    // for (int id = 0x201; id < 0x205; ++id)
+    // {
+    //     if (can.read(msg) == id)
+    //     {
+    //         if (id == 0x201)
+    //         {
+    //             speed_now = (msg.data[0] << 8) | msg.data[1];
+    //         }
+    //         else if (id == 0x202)
+    //         {
+    //             speed_now = (msg.data[2] << 8) | msg.data[3];
+    //         }
+    //         else if (id == 0x203)
+    //         {
+    //             speed_now = (msg.data[4] << 8) | msg.data[5];
+    //         }
+    //         else if (id == 0x204)
+    //         {
+    //             speed_now = (msg.data[6] << 8) | msg.data[7];
+    //         }
+    //     }
+    // }
+    // if (can.read(msg) == 0x201)
+    // {
+    //     speed_now = (msg.data[2] << 8) | msg.data[3];
+    //     printf("speed_now=:%d\n", speed_now);
+    // }
+
+    // float output = pid_controller.calculate(target, speed_now); // PID計算
+    // int16_t motor_speed = static_cast<int16_t>(-output);        // 出力値を速度として使用
+    // DATA[0] = (motor_speed >> 8) & 0xFF;                        // 上位バイト
+    // DATA[1] = motor_speed & 0xFF;                               // 下位バイト
+    // motor_speed = static_cast<int16_t>(output);
+    // DATA[2] = (motor_speed >> 8) & 0xFF; // 上位バイト
+    // DATA[3] = motor_speed & 0xFF;        // 下位バイト
+    // motor_speed = static_cast<int16_t>(-output);
+    // DATA[4] = (motor_speed >> 8) & 0xFF; // 上位バイト
+    // DATA[5] = motor_speed & 0xFF;        // 下位バイト
+    // motor_speed = static_cast<int16_t>(output);
+    // DATA[6] = (motor_speed >> 8) & 0xFF; // 上位バイト
+    // DATA[7] = motor_speed & 0xFF;        // 下位バイト
+    // return DATA;
 }
+
 int main()
 {
     button_reset.mode(PullUp);
@@ -67,14 +91,34 @@ int main()
         if (now - pre > 1000ms && sw == 0)
         {
             printf("FIRE\n");
-            target = 16000;                        // 目標速度を設定
+            // 目標速度を設定
             auto start_time = HighResClock::now(); // PID計算開始時刻を記録
 
             while (HighResClock::now() - start_time < 1000ms) // 1秒間PID計算を行う
             {
-                void motor_control(int16_t target, int16_t speed_now, PID & pid_controller, CAN & can, CANMessage & msg, uint8_t * DATA, int DJI_ID, bool &flag);
 
+                // if (msg.id == 0x201)
+                // {
+                //     speed_now = (msg.data[2] << 8) | msg.data[3];
+                //     printf("speed_now=:%d\n", speed_now);
+                // }
+                target = 8000;
+                float output = pid_controller.calculate(target, speed_now); // PID計算
+                int16_t motor_speed = static_cast<int16_t>(-output);        // 出力値を速度として使用
+                DATA[0] = (motor_speed >> 8) & 0xFF;                        // 上位バイト
+                DATA[1] = motor_speed & 0xFF;                               // 下位バイト
+                motor_speed = static_cast<int16_t>(output);
+                DATA[2] = (motor_speed >> 8) & 0xFF; // 上位バイト
+                DATA[3] = motor_speed & 0xFF;        // 下位バイト
+                motor_speed = static_cast<int16_t>(-output);
+                DATA[4] = (motor_speed >> 8) & 0xFF; // 上位バイト
+                DATA[5] = motor_speed & 0xFF;        // 下位バイト
+                motor_speed = static_cast<int16_t>(output);
+                DATA[6] = (motor_speed >> 8) & 0xFF; // 上位バイト
+                DATA[7] = motor_speed & 0xFF;        // 下位バイト
+                speed_now = motor_speed;
                 CANMessage msg(DJI_ID, DATA, 8);
+                printf("motor_speed=:%d\n", motor_speed);
                 if (can.write(msg))
                 {
                     printf("PID Output Sent\n");
@@ -84,9 +128,6 @@ int main()
                     printf("Can't send Message\n");
                     can.reset();
                 }
-
-                // 速度センサーからの読み取り値を更新する場合はここで行う
-                // speed_now = <センサーからの読み取り値>;
 
                 ThisThread::sleep_for(20ms); // PID計算のサンプル時間に合わせて待機
             }
